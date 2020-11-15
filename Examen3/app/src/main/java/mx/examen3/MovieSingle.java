b507a4529902dfeea9a3bcab85c370a797156ed6
r2d2 <tealflipper@gmail.com>
2020-11-15 08:03:40 -0600

    images can be seen

---
+++ Examen3/app/src/main/java/mx/examen3/MovieSingle.java
@@ -0,0 +1,16 @@
+package mx.examen3;
+
+import android.graphics.Bitmap;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class MovieArray {
+    ArrayList<String> movieList;
+    ArrayList<Bitmap> movieImages;
+
+    public MovieArray(){
+
+    }
+
+}
